1dd09dbf11a3a4ce2f417e7c199c583e4e929ed4
Kingfree <anytjf@gmail.com>
2015-06-23 13:24:43 +0800

    Initial Commit

---
+++ muse/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  muse
+//
+//  Created by 咲衣憧 on 15/6/23.
+//  Copyright (c) 2015年 Kingfree. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ muse/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  muse
+//
+//  Created by 咲衣憧 on 15/6/23.
+//  Copyright (c) 2015年 Kingfree. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
